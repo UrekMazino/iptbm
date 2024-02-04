76c6b44595285067fa444a2919dc8c4086ab099b
andy mark servania <andymarkservania1991@gmail.com>
2024-02-04 23:49:10 +0800

    jhgjhgooo

--- app/Http/Livewire/Iptbm/AdminDashboard/TotalTechProtocol.php
+++ app/Http/Livewire/Iptbm/AdminDashboard/TotalTechProtocol.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Iptbm\AdminDashboard;
 
 use App\Models\iptbm\IptbmProfile;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class TotalTechProtocol extends Component
@@ -11,7 +12,7 @@ class TotalTechProtocol extends Component
 
     public function render()
     {
-        $this->profiles = IptbmProfile::all();
+        $this->profiles = IptbmProfile::with('agency.region')->where('techno_transfer', '1')->get();
         return view('livewire.iptbm.admin-dashboard.total-tech-protocol')->with([
             'profiles' => $this->profiles
         ]);
