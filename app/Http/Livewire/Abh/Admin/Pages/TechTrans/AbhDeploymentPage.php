b39e74c3fe7c0b27cee2194d0189aae3b8c9e27c
andy mark servania <andymarkservania1991@gmail.com>
2024-02-29 15:06:50 +0800

    latest update feb 29

--- app/Http/Livewire/Abh/Admin/Pages/TechTrans/AbhDeploymentPage.php
+++ app/Http/Livewire/Abh/Admin/Pages/TechTrans/AbhDeploymentPage.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\Abh\Admin\Pages\TechTrans;
 
 use App\View\Components\abh\admin\AbhAdminApp;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Livewire\Component;
 
 class AbhDeploymentPage extends Component
 {
-    public function render(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View|\Illuminate\Contracts\Foundation\Application
+    public function render(): View|Application|Factory|\Illuminate\View\View|\Illuminate\Contracts\Foundation\Application
     {
         return view('livewire.abh.admin.pages.techtrans.abh-deployment-page')
             ->layout(AbhAdminApp::class);
