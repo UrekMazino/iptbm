c249695c8c052812b23689435292a9efb6f947b3
andy mark servania <andymarkservania1991@gmail.com>
2024-02-29 17:51:19 +0800

    update feb pm artisan

--- resources/views/admin/abh/layout/app.blade.php
+++ resources/views/admin/abh/layout/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ABH: {{ config('app.name', 'Laravel') }}
-
+    <title>
+        ABH: {{ config('app.name', 'Laravel') }}
         @yield('title')
     </title>
 
